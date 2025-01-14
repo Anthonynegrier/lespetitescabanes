bcac2486800f1e815706ef0b2b26b06d66538a8b
Anthonynegrier <anthony.negrier3@gmail.com>
2025-01-14 08:44:39 +0100

    Creation du consulter.html.twig + Ajout du lister et controller + ajout d'info dans le Creche.php

---
+++ migrations/Version20250114071246.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250114071246 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE creche ADD ville VARCHAR(255) NOT NULL, ADD copos INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE creche DROP ville, DROP copos');
+    }
+}
--- src/Entity/Creche.php
+++ src/Entity/Creche.php
@@ -16,6 +16,24 @@ class Creche
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    #[ORM\Column]
+    private ?int $capaccite = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contact = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    #[ORM\Column]
+    private ?int $copos = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +47,78 @@ class Creche
     public function setNom(string $nom): static
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCapaccite(): ?int
+    {
+        return $this->capaccite;
+    }
+
+    public function setCapaccite(int $capaccite): static
+    {
+        $this->capaccite = $capaccite;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(string $contact): static
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCopos(): ?int
+    {
+        return $this->copos;
+    }
+
+    public function setCopos(int $copos): static
+    {
+        $this->copos = $copos;
 
         return $this;
     }
